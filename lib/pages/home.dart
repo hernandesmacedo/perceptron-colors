4d96ac8e80b67becdc043e40f96a7a83d555e94c
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-26 19:26:00 -0300

    Colors now change according to sliders' values

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -61,7 +61,12 @@ class _HomePageState extends State<HomePage> {
               Container(
                 height: (MediaQuery.of(context).size.height) * 0.1,
                 width: (MediaQuery.of(context).size.height) * 0.1,
-                color: Colors.red,
+                color: Color.fromRGBO(
+                  rSliderValue,
+                  gSliderValue,
+                  bSliderValue,
+                  1,
+                ),
               )
             ],
           ),
