f398ac93d421e5211e63b5a5464aafc08717c47b
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-26 20:46:39 -0300

    ClipRRect added in order to mold container radius

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,55 +20,61 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: const Header(),
       body: Center(
-        child: Container(
-          width: (MediaQuery.of(context).size.height) * 0.65,
-          height: (MediaQuery.of(context).size.height) * 0.65,
-          color: const Color(0xFFedfffd),
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RGBSlider(
-                min: 0,
-                max: 255,
-                value: rSliderValue,
-                onChanged: (value) {
-                  setState(() {
-                    rSliderValue = value.toInt();
-                  });
-                },
-              ),
-              RGBSlider(
-                min: 0,
-                max: 255,
-                value: gSliderValue,
-                onChanged: (value) {
-                  setState(() {
-                    gSliderValue = value.toInt();
-                  });
-                },
-              ),
-              RGBSlider(
-                min: 0,
-                max: 255,
-                value: bSliderValue,
-                onChanged: (value) {
-                  setState(() {
-                    bSliderValue = value.toInt();
-                  });
-                },
-              ),
-              Container(
-                height: (MediaQuery.of(context).size.height) * 0.1,
-                width: (MediaQuery.of(context).size.height) * 0.1,
-                color: Color.fromRGBO(
-                  rSliderValue,
-                  gSliderValue,
-                  bSliderValue,
-                  1,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            width: (MediaQuery.of(context).size.height) * 0.65,
+            height: (MediaQuery.of(context).size.height) * 0.65,
+            color: const Color(0xFFedfffd),
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RGBSlider(
+                  min: 0,
+                  max: 255,
+                  value: rSliderValue,
+                  onChanged: (value) {
+                    setState(() {
+                      rSliderValue = value.toInt();
+                    });
+                  },
                 ),
-              )
-            ],
+                RGBSlider(
+                  min: 0,
+                  max: 255,
+                  value: gSliderValue,
+                  onChanged: (value) {
+                    setState(() {
+                      gSliderValue = value.toInt();
+                    });
+                  },
+                ),
+                RGBSlider(
+                  min: 0,
+                  max: 255,
+                  value: bSliderValue,
+                  onChanged: (value) {
+                    setState(() {
+                      bSliderValue = value.toInt();
+                    });
+                  },
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    height: (MediaQuery.of(context).size.height) * 0.1,
+                    width: (MediaQuery.of(context).size.height) * 0.1,
+                    color: Color.fromRGBO(
+                      rSliderValue,
+                      gSliderValue,
+                      bSliderValue,
+                      1,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
